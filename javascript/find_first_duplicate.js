88479eb6cc080cce12628ed4769e0a4e1e163d0b
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-10-11 14:36:00 -0600

    pseudocode

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -16,4 +16,25 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+/**
+ * given an array of integers, find the first instance of a repeated number, or return -1 if none
+ * min n time complexity? have to go through the whole array once excepting edge cases
+ * where theres a repeat in the beginning of the array, then the case is trivial anyway
+ * 
+ * iterate over the array, storing unique values
+ * compare each value in the array to the previous uniques, if it matches
+ * return that value
+ * if you go through the whole array without finding a match
+ * return -1
+ */
 // And a written explanation of your solution
+/**
+ * theres definitely a more clever solution to this, but brute forcing seems to be fine
+ * with a problem this simple. Iterating over the whole array is an easy way to find a double,
+ * theres another easier to write solution that would be ~equal or less efficient depending
+ * on the language written in, making a set first and then if the set is shorter than the
+ * array, the array has doubles.
+ * however, iterating over and making a new array to compare as you go works just fine, 
+ * assuming the test cases dont have an array that is larger than 1E+9 or something
+ * considering how fast the clone went with my slow internet, it does not
+ */
